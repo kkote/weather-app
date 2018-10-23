d8c3b159767fd3e329789f8af46e8e7d09b0eea8
Katherine Kote <kaygray521@gmail.com>
2018-10-23 00:00:00 +0000

    adding key files with env

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import React from 'react';
+import Weather from "./components/weather";
+import Form from "./components/form";
+import Titles from "./components/titles";
 
-export default App;
+
+    class App extends React.Component {
+
+        getWeather = async () => {
+            const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${Api_Key}`);
+            const response = await api_call.json();
+            console.log(response);
+        }
+
+
+        render() {
+            return (
+                <div>
+                    <Titles />
+                    <Form />
+                    <Weather />
+                </div>
+            )
+        }
+
+    }
+    export default App;
---
+++ src/components/titles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Titles = (props) => {
+    return (
+        <div>
+            <h1>Weather App</h1>
+            <p>Helps you find weather in your city</p>
+        </div>
+    )
+}
+
+export default Titles;
