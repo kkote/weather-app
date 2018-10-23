ae0b99028e946f2eb70494bf1d0045559e9a064d
Katherine Kote <kaygray521@gmail.com>
2018-10-23 00:00:00 +0000

    setting up env

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
 import React from 'react';
 import Weather from "./components/weather";
 import Form from "./components/form";
 import Titles from "./components/titles";
+require('dotenv').config();
+const apikey  = process.env.API_KEY;
 
 
     class App extends React.Component {
 
         getWeather = async () => {
-            const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${Api_Key}`);
+            const api_call = await fetch(`https://api.darksky.net/forecast/${apikey}/37.8267,-122.4233`);
             const response = await api_call.json();
             console.log(response);
         }
