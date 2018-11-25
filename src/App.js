ce7bcef55e39335c90a4c529b8cee134ca6a6969
Katherine Kote <kaygray521@gmail.com>
2018-11-25 00:00:00 +0000

    editing functions

--- src/App.js
+++ src/App.js
@@ -1,225 +1,176 @@
 import React from "react";
 import Weather from "./components/weather";
 import Style from "./components/styleguide";
-import { Grid, Cell } from "react-flexr";
+import {Grid, Cell} from "react-flexr";
 import "react-flexr/styles.css";
 
 let Api_Key = process.env.REACT_APP_API_KEY;
-// const longitude =  "-90.2";
-// const latitude = "38.6";
-// const weatherUrl = ("api.openweathermap.org/data/2.5/weather?lat="   + latitude + "&lon=" + longitude + "&appid=" + Api_Key) ;
-// console.log(weatherUrl);
 
-const Titles = props => {
-	return (
-		<div>
-			<h1 className="title-container__title">Fashion Forecast</h1>
-			<div className="title-container__subtitle ">Weather Style Guide</div>
-		</div>
-	);
+const Title = props => {
+  return (<div clasName="title-boxes">
+    <h1 className="title">Fashion Forecast</h1>
+    <div className="subtitle ">Weather Style Guide</div>
+  </div>);
 };
 
 const Form = props => {
-	return (
-		<Grid>
-			<Cell>
-				<form onSubmit={props.loadWeather}>
-					<Cell className="location-search" size="2/3">
-						<input type="text" name="city" placeholder="city" />
-					</Cell>
-					{/* <input type="text" name="country" placeholder="country"/> */}
-					<Cell className="location-search">
-						<button>Search</button>
-					</Cell>
-				</form>
-			</Cell>
-		</Grid>
-	);
+  return (
+    <Cell>
+      <form onSubmit={props.loadWeather}>
+        <Cell className="location-search" size="2/3">
+          <input type="text" name="city" placeholder="city"/>
+        </Cell>
+        {/* <input type="text" name="country" placeholder="country"/> */}
+        <Cell className="location-search">
+          <button>Search</button>
+        </Cell>
+      </form>
+    </Cell>
+  );
 };
-
-
 
 //        var aImg = "couple";
 //
 //        var imageName = require('./Images/' + aImg + '.jpg');
 
 class Img extends React.Component {
-	render() {
-		return (
-			<div className="img-box">
-			<img src={this.props.img} />
-			</div>
-		);
-	}
+  render() {
+    return (<div className="img-box">
+      <img src={this.props.img}/>
+    </div>);
+  }
 }
-
 
 class App extends React.Component {
-	state = {
-		temperature: " -  -",
-		city: "_______",
-		country: "__",
-		description: "_____",
-		mintemp: " -  -",
-		maxtemp: " -   -",
-		icon: " ",
-		error: undefined,
-		outer: " ",
-		top: " ",
-		pants: " ",
-		shoes: " ",
-		img: require('./Images/winter-couplesize.jpg')
-	};
+  state = {
+    temperature: " -  -",
+    city: "_______",
+    country: "__",
+    description: "_____",
+    mintemp: " -  -",
+    maxtemp: " -   -",
+    icon: " ",
+    error: undefined,
+    outer: " ",
+    top: " ",
+    pants: " ",
+    shoes: " ",
+    img: require('./Images/winter-couplesize.jpg')
+  };
 
-	getWeather = async e => {
-		//		 const city = e.target.elements.city.value;
-		// const country = e.target.elements.country.value;
+  getWeather = async e => {
+    //		 const city = e.target.elements.city.value;
+    // const country = e.target.elements.country.value;
+    const city = "St. Louis";
+    const country = "us";
 
-		const city = "St. Louis";
-		const country = "us";
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&appid=${Api_Key}`);
 
-		e.preventDefault();
-		const api_call = await fetch(
-			`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&appid=${Api_Key}`
-		);
+    console.log(api_call);
+    const response = await api_call.json();
+    console.log(response);
 
-		console.log(api_call);
-		const response = await api_call.json();
-		console.log(response);
-		{
-			/*var iconcode = response.weather[0].icon;
-         const iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";*/
-		}
-		const tempNum = parseInt(response.main.temp, 10);
-		const tempNumHi = parseInt(response.main.temp_max, 10);
-		const tempNumLo = parseInt(response.main.temp_min, 10);
+    const tempNum = parseInt(response.main.temp, 10);
+    const tempNumHi = parseInt(response.main.temp_max, 10);
+    const tempNumLo = parseInt(response.main.temp_min, 10);
 
-		console.log(tempNum);
+    console.log(tempNum);
 
-		if (city && country) {
-			this.setState({
-				temperature: tempNum,
-				city: response.name,
-				country: response.sys.country,
-				description: response.weather[0].description,
-				mintemp: tempNumLo,
-				maxtemp: tempNumHi,
-				error: ""
-			});
-		} else {
-			this.setState({
-				error: "Please input search"
-			});
-		}
+    if (city && country) {
+      this.setState({
+        temperature: tempNum,
+        city: response.name,
+        description: response.weather[0].description,
+        mintemp: tempNumLo,
+        maxtemp: tempNumHi,
+        error: ""
+      });
+    } else {
+      this.setState({error: "Please input search"});
+    }
 
-		if (tempNum >= 75) {
-			this.setState({
-				outer: "None",
-				top: "T-shirt",
-				pants: "Shorts",
-				shoes: "Sandals",
-				img: require('./Images/summer-groups.jpg'),
-				error: ""
-			});
-		} else if ((tempNum >= 60) & (tempNum < 75)) {
-			this.setState({
-				outer: "Hoodie or Light Jacket",
-				top: "T-shirt",
-				pants: "Shorts or Pants",
-				shoes: "Shoes ",
-				img: require('./Images/spring-group-s.jpg'),
-				error: ""
-			});
-		} else if ((tempNum >= 50) & (tempNum < 60)) {
-			this.setState({
-				outer: "Jacket/Fleece",
-				top: "T-shirt or Long sleeve",
-				pants: "Pants",
-				shoes: "Shoes",
-				img: "",
-				img: require('./Images/fall-couple-s.jpg'),
-				error: ""
-			});
-		} else if ((tempNum >= 40) & (tempNum < 50)) {
-			this.setState({
-				outer: "Jacket/Fleece ",
-				top: " Long Sleeve",
-				pants: "Pants",
-				shoes: "Shoes or Boots",
-				img: "",
-				img: require('./Images/winter-couplesize.jpg'),
-				error: ""
+    if (tempNum >= 75) {
+      this.setState({
+        outer: "None",
+        top: "T-shirt",
+        pants: "Shorts",
+        shoes: "Sandals",
+        img: require('./Images/summer-groups.jpg'),
+        error: ""
+      });
+    } else if ((tempNum >= 60) & (tempNum < 75)) {
+      this.setState({
+        outer: "Hoodie or Light Jacket",
+        top: "T-shirt",
+        pants: "Shorts or Pants",
+        shoes: "Shoes ",
+        img: require('./Images/spring-group-s.jpg'),
+        error: ""
+      });
+    } else if ((tempNum >= 50) & (tempNum < 60)) {
+      this.setState({
+        outer: "Jacket/Fleece",
+        top: "T-shirt or Long sleeve",
+        pants: "Pants",
+        shoes: "Shoes",
+        img: "",
+        img: require('./Images/fall-couple-s.jpg'),
+        error: ""
+      });
+    } else if ((tempNum >= 40) & (tempNum < 50)) {
+      this.setState({
+        outer: "Jacket/Fleece ",
+        top: " Long Sleeve",
+        pants: "Pants",
+        shoes: "Shoes or Boots",
+        img: "",
+        img: require('./Images/winter-couplesize.jpg'),
+        error: ""
 
-			});
-		} else {
-			this.setState({
-				outer: "Layers: Hoodie/Jacket/Coat",
-				top: "Long Sleeve",
-				pants: "Thick Pants or Layers",
-				shoes: "Boots",
-				img: require('./Images/winter-woman-crop2size.jpg')
-			});
-		}
+      });
+    } else {
+      this.setState({outer: "Layers: Hoodie/Jacket/Coat", top: "Long Sleeve", pants: "Thick Pants or Layers", shoes: "Boots", img: require('./Images/winter-woman-crop2size.jpg')});
+    }
 
+  };
 
-
-	};
-
-
-	render() {
-		return (
-			<div>
-				<Grid>
-					<Cell className="nav-box">
-						<Cell className="title-box" palm="hidden" size="1/4">
-							<Titles />
-						</Cell>
-						{/*{<Cell className="search-form">
+  render() {
+    return (<div>
+      <Grid>
+        <Cell className="nav-box">
+          <Cell className="title-box" palm="hidden" size="1/4">
+            <Title/>
+          </Cell>
+          {/*{<Cell className="search-form">
 							<Form loadWeather={this.getWeather} />
-						</Cell> }*/ }
-						<Cell className="weather-top" palm="2/3">
-							<Cell className="search-form">
-								<Form loadWeather={this.getWeather} />
-							</Cell>
+						</Cell> }*/
+          }
+          <Cell className="weather-top" palm="2/3">
+            <Cell className="search-form">
+              <Form loadWeather={this.getWeather}/>
+            </Cell>
 
-							<Cell   size="3/5">
-								<Weather
-									temperature={this.state.temperature}
-									city={this.state.city}
-									country={this.state.country}
-									description={this.state.description}
-									maxtemp={this.state.maxtemp}
-									mintemp={this.state.mintemp}
-									error={this.state.error}
-									icon={this.state.icon}
-								/>
-							</Cell>
-						</Cell>
-					</Cell>
-				</Grid>
+            <Cell size="3/5">
+              <Weather temperature={this.state.temperature} city={this.state.city} country={this.state.country} description={this.state.description} maxtemp={this.state.maxtemp} mintemp={this.state.mintemp} error={this.state.error} icon={this.state.icon}/>
+            </Cell>
+          </Cell>
+        </Cell>
+      </Grid>
 
-				<div className="wrapper">
-					<div className="main">
-						<div className="display-container">
-							<Grid className="display-container">
-								<Cell className="style-box" size="1/2">
-									<Style
-										top={this.state.top}
-										pants={this.state.pants}
-										outer={this.state.outer}
-										shoes={this.state.shoes}
-										error={this.state.error}
-									/>
-								</Cell>
-								<Img img={this.state.img} />
+      <div className="wrapper">
+        <div className="main">
+            <Grid className="display-container">
+              <Cell className="style-box" size="1/2">
+                <Style top={this.state.top} pants={this.state.pants} outer={this.state.outer} shoes={this.state.shoes} error={this.state.error}/>
+              </Cell>
+              <Img img={this.state.img}/>
 
-							</Grid>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+            </Grid>
+
+        </div>
+      </div>
+    </div>);
+  }
 }
 export default App;
-
--- src/Apps.js
+++ src/Apps.js
@@ -1,104 +1,299 @@
 import React from "react";
+import Weather from "./components/weather";
+import Style from "./components/styleguide";
+
 
 let Api_Key = process.env.REACT_APP_API_KEY;
 
 const Header = props => {
-return(
-<div>
-  <h1 className="title">
-    Fashion Forecast
-  </h1>
-  <h3 className="subtitle">
-  Weather Style Guide
-  </h3>
-  </div>
-
-);
-
-}
-
-class Weather extends React.Component {
-    render() {
-        return (
-            <div>
-            {this.props.temperature}
-            </div>
-
-        )
-    }
+  return (
+    <div>
+    <h1 className="title">
+      Fashion Forecast
+    </h1>
+    <h3 className="subtitle">
+      Weather Style Guide
+    </h3>
+  </div>);
 }
 
 
-class LocationForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
+class Search extend conponent {
+  <div>
+  <form onSubmit={props.loadWeather(Weather Component)}>
+    <div className="location-search" size="2/3">
+      <input type="text" name="city" placeholder="city"/>
+    </div>
+      <button>Search</button>
+  </form>
+    <div>
+}
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
 
 
+
+
+ const Display = props => {
+   return (
+     <div className="weather-display">
+       {  (
+         <div className="displayGroup">
+         <div>
+         In
+         <span className="location">
+           {" "}{this.props.city}{" "}
+         </span>
+         </div>
+           <div>
+           Its currently
+           <span id="curentTemp" className=""> {this.props.currentTemp} ° F</span>
+           With
+           <span className="description"> {this.props.currentWeather}
+           </span>
+           </div>
+
+         </div>
+       )}
+       { (<div id="hiLoTemp" className="tempRangedisplayGroup">
+           <span className="temprange"> High</span>
+           <span className="tempoutput">
+           {this.props.hiTemp} ° F</span>
+           <span className="temprange"> Low</span>
+           <span className="tempoutput"> {this.props.loTemp} °F</span>
+         </div>
+       )}
+     </div>
+   );
+ }
+
+
+
+takes temp props
+class Style extends Component {
+  constructor() {
+    super();
+    this.state = {
+      top: null,
+      pants: false,
+      outer: false,
+      shoes: false,
+      tempNum: {this.props.currentTemp},
+      error: null
+    };
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-
-  }
-
-  handleSubmit(event) {
-
-		const city = "St. Louis";
-		const country = "us";
-
-		event.preventDefault();
-		const api_call = fetch(
-			`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&appid=${Api_Key}`
-		).then(response => response.json())
-		.then(json => this.setState({ value : json }));
-		console.log(api_call);
-
-
-
-
-    console.log('Location: ' + this.state.value);
-
+  if (tempNum >= 75) {
+    this.setState({
+      outer: "None",
+      top: "T-shirt",
+      pants: "Shorts",
+      shoes: "Sandals"
+    });
+  } else if ((tempNum >= 60) & (tempNum < 75)) {
+    this.setState({
+      outer: "Hoodie or Light Jacket",
+      top: "T-shirt",
+      pants: "Shorts or Pants",
+      shoes: "Shoes "
+    });
+  } else if ((tempNum >= 50) & (tempNum < 60)) {
+    this.setState({
+      outer: "Jacket/Fleece",
+      top: "T-shirt or Long sleeve",
+      pants: "Pants",
+      shoes: "Shoes"
+    });
+  } else if ((tempNum >= 40) & (tempNum < 50)) {
+    this.setState({
+      outer: "Jacket/Fleece ",
+      top: " Long Sleeve",
+      pants: "Pants",
+      shoes: "Shoes or Boots"
+    });
+  } else {
+    this.setState({
+    outer: "Layers: Hoodie/Jacket/Coat",
+    top: "Long Sleeve",
+    pants: "Thick Pants or Layers",
+    shoes: "Boots"});
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Location:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div className="clothesDisplay">
+        {(<div >
+            <span className="clothesType"> Outer: </span>
+            <span > {this.state.outer}</span>
+          </div>
+        )}
+        {(<div className="style-div">
+            <span className="clothesType"> Top: </span>
+            <span className="style-result"> {this.props.top}</span>
+          </div>
+        )}
+        {(<div className="style-div">
+            <span className="clothesType"> Pants: </span>
+            <span className="style-result"> {this.props.pants}</span>
+          </div>
+        )}
+        {(<div className="style-div">
+            <span className="clothesType"> Shoes: </span>
+            <span className="style-result"> {this.props.shoes}</span>
+          </div>
+        )}
+      </div>
     );
   }
 }
 
 
 
-class Apps extends React.Component{
+class Img extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gender: undefined,
+      img: require('./Images/winter-couplesize.jpg'),
+      tempNum: {this.props.currentTemp},
+      error: null
+    };
+  }
 
-
-render(){
-    return (
-        <div>
-        <Header />
-        <Header />
-        <LocationForm loadWeather={this.getWeather} />
-        <Weather   temperature={this.props.value} />
-        </div>
-
-
-        );
-    }
-
+  if (tempNum >= 75) {
+    this.setState({
+      img: require('./Images/summer-groups.jpg'),
+    });
+  } else if ((tempNum >= 60) & (tempNum < 75)) {
+    this.setState({
+      img: require('./Images/spring-group-s.jpg'),
+    });
+  } else if ((tempNum >= 50) & (tempNum < 60)) {
+    this.setState({
+      img: require('./Images/fall-couple-s.jpg'),
+    });
+  } else if ((tempNum >= 40) & (tempNum < 50)) {
+    this.setState({
+      img: require('./Images/winter-couplesize.jpg'),
+    });
+  } else {
+    this.setState({
+    img: require('./Images/winter-woman-crop2size.jpg')});
+  }
+  render() {
+    return (<div className="img-box">
+      <img src={this.props.img}/>
+    </div>);
+  }
 }
 
-export default Apps;
+
+const Search = props => {
+  return (
+    <div>
+    <form onSubmit={props.loadWeather(Weather Component)}>
+      <div className="location-search" size="2/3">
+        <input type="text" name="city" placeholder="city"/>
+      </div>
+        <button>Search</button>
+    </form>
+      <div>);
+}
 
 
 
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      city: "St. Louis",
+      country: "us",
+      currentTemp: "",
+      weather: "",
+      hiTemp: "",
+      loTemp: "",
+      isLoaded: false,
+      click: false,
+      data: [],
+      error: null
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+   this.setState({
+     data: event.target.value
+   });
+ }
+
+
+
+  handleSubmit(event){}
+   componentDidMount() {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},us&units=imperial&appid=${Api_Key}`)
+  .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              currentTemp: parseInt(response.main.temp, 10),
+              isLoaded: true,
+              city: result.name,
+              data: result,
+              description: result.weather[0].description,
+              hiTemp: parseInt(result.main.temp, 10),
+              loTemp: parseInt(result.main.temp, 10)
+            });
+            console.log(this.state.data);
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    };
+
+  render() {
+    return (
+  <div className="App">
+      <div class= "header">
+      <Header />
+
+      <Search loadWeather={this.getWeather}/>
+      <Search key={event} loadWeather={handleSubmit}/>
+
+      <Display
+        city={this.state.city}
+        currentTemp={this.state.currentTemp}
+        currentWeather={this.state.weather}
+        hiTemp={this.state.hiTemp} hiTemp={this.state.loTemp}
+         />
+      </div>
+    <div class="styledisplay">
+    <Style
+      currentTemp={this.state.currentTemp}
+      />
+    <Img currentTemp={this.state.currentTemp}/>
+    </div>
+</div>
+    )
+  }
+}
+
+const Search = props => {
+  return (
+    <div>
+    <form onSubmit={this.handleSubmit}>
+      <div className="location-search" size="2/3">
+        <input type="text" name="city" placeholder="city"/>
+      </div>
+        <button>Search</button>
+    </form>
+      <div>);
+}
+
+
+
+
+
+export default App;
