cda21dabc650afefa61f05ffece0ade66917fe85
Katherine Kote <kaygray521@gmail.com>
2018-11-28 00:00:00 +0000

    search function

--- src/Apps.js
+++ src/Apps.js
@@ -9,87 +9,6 @@ import Header from "./components/header";
 let Api_Key = process.env.REACT_APP_API_KEY;
 
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      city: "St. Louis",
-      country: "us",
-      currentTemp: "",
-      weather: "",
-      hiTemp: "",
-      loTemp: "",
-      isLoaded: false,
-      click: false,
-      data: [],
-      error: null
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
-   this.setState({
-     data: event.target.value
-   });
- }
-
-
-
-  handleSubmit(event){}
-   componentDidMount() {
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},us&units=imperial&appid=${Api_Key}`)
-  .then(res => res.json())
-        .then(
-          (result) => {
-            this.setState({
-              currentTemp: parseInt(response.main.temp, 10),
-              isLoaded: true,
-              city: result.name,
-              data: result,
-              description: result.weather[0].description,
-              hiTemp: parseInt(result.main.temp, 10),
-              loTemp: parseInt(result.main.temp, 10)
-            });
-            console.log(this.state.data);
-          },
-          (error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
-          }
-        )
-    };
-
-  render() {
-    return (
-  <div className="App">
-      <div class= "header">
-      <Header />
-
-      <Search loadWeather={this.getWeather}/>
-      <Search key={event} loadWeather={handleSubmit}/>
-
-      <Display
-        city={this.state.city}
-        currentTemp={this.state.currentTemp}
-        currentWeather={this.state.weather}
-        hiTemp={this.state.hiTemp} hiTemp={this.state.loTemp}
-         />
-      </div>
-    <div class="styledisplay">
-    <Style
-      currentTemp={this.state.currentTemp}
-      />
-    <Img currentTemp={this.state.currentTemp}/>
-    </div>
-</div>
-    )
-  }
-}
-
-
-
-
 const App = () => {
   return (
 <div className="App">
@@ -119,7 +38,109 @@ const App = () => {
 
 
 
-
-
-
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDataChange = this.handleDataChange.bind(this);
+    this.state = {
+      data: '',
+      city: 'St. Louis',
+      currentTemp="",
+      currentWeather="",
+      hiTemp="",
+      hiTemp= "",
+      isLoaded: false,
+      error: null
+    };
+  }
+
+
+  handleDataChange(city) {
+    this.setState({
+      city
+    });
+  }
+
+
+  componentDidMount() {
+ fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},us&units=imperial&appid=${Api_Key}`)
+ .then(res => res.json())
+       .then(
+         (result) => {
+           this.setState({
+             currentTemp: parseInt(response.main.temp, 10),
+             isLoaded: true,
+             city: result.name,
+             data: result,
+             description: result.weather[0].description,
+             hiTemp: parseInt(result.main.temp_max, 10),
+             loTemp: parseInt(result.main.temp_min, 10)
+           });
+           console.log(this.state.data);
+         },
+         (error) => {
+           this.setState({
+             isLoaded: true,
+             error
+           });
+         }
+       )
+   };
+
+
+
+  render() {
+
+    const temperature = this.state.temperature;
+    const temp = this.state.currentTemp ;
+    const hightemp = this.state.hiTemp ;
+    const lowtemp = this.state.loTemp ;
+    const currentForecast = this.state.currentWeather ;
+
+
+    return (
+      <div className="App">
+
+      <div className="header">
+        <Search
+          temperature={temp}
+          onCityChange={this.handleDataChange} />
+
+          <Display
+            city={this.state.city}
+            currentTemp={temp}
+            currentWeather={currentForecast}
+            hiTemp={hightemp}
+            hiTemp={lowtemp}
+             />
+         </div>
+
+             <div class="styledisplay">
+             <Style
+               currentTemp={temp}
+               />
+             <Img currentTemp={temp}/>
+             </div>
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Calculator />,
+  document.getElementById('root')
+);
--- src/components/search.js
+++ src/components/search.js
@@ -1,9 +1,6 @@
 
 import React from 'react';
 
-let Api_Key = process.env.REACT_APP_API_KEY;
-
-
 
 class Search extends React.Component {
   render() {
@@ -12,6 +9,15 @@ class Search extends React.Component {
       <form onSubmit={props.loadWeather(Weather Component)}>
         <div className="location-search" size="2/3">
           <input type="text" name="city" placeholder="city"/>
+
+
+
+          <input
+         value={temperature}
+         onChange={this.handleChange} />
+
+
+
         </div>
           <button>Search</button>
       </form>
@@ -22,52 +28,4 @@ class Search extends React.Component {
 
 
 
-const Search = props => {
-  return (
-    <div>
-    <form onSubmit={props.loadWeather(Weather Component)}>
-      <div className="location-search" size="2/3">
-        <input type="text" name="city" placeholder="city"/>
-      </div>
-        <button>Search</button>
-    </form>
-      <div>);
-}
-
-
-const Search = props => {
-  return (
-    <div>
-    <form onSubmit={this.handleSubmit}>
-      <div className="location-search" size="2/3">
-        <input type="text" name="city" placeholder="city"/>
-      </div>
-        <button>Search</button>
-    </form>
-      <div>);
-}
-
-
-
 export default Search;
-
-
-
-
-
-
-import React from 'react';
-
-const Form = (props) => {
-    return (
-
-        <form onSubmit = {props.loadWeather}>
-        <input type="text" name="city" placeholder="city" />
-        <input type="text" name="country" placeholder="country"/>
-            <button>Get Weather</button>
-        </form>
-
-    )
-}
-
-export default Form;
