88871f8c21690350ac5fd11cd18a5bf004fdc6f1
Katherine Kote <kaygray521@gmail.com>
2018-12-07 00:00:00 +0000

    functions
    male/female image changes with selectione

--- src/components/img.js
+++ src/components/img.js
@@ -20,39 +20,57 @@ this.setStyle = this.setStyle.bind(this);
 
 
 
+
+
+
+  setStyle(){
+  let currentTemp = this.props.currentTemp;
+  let gender = this.props.gender;
+  console.log({currentTemp});
+  if (currentTemp >= 75) {
+    this.setState({
+    img: require('../Images/defaultImg/summer-groups.jpg')
+    });
+  } else if ((currentTemp >= 60) & (currentTemp < 75)) {
+    this.setState({
+        img: require('../Images/defaultImg/spring-group-s.jpg')
+    });
+  } else if ((currentTemp >= 50) & (currentTemp < 60)) {
+    this.setState({
+      img: require('../Images/defaultImg/fall-couple-s.jpg')
+    });
+  } else if ((currentTemp >= 40) & (currentTemp < 50)) {
+    this.setState({
+      img: require('../Images/defaultImg/winter-couplesize.jpg')
+    });
+  } else {
+    if (gender === "female"){
+      this.setState({
+        img: require('../Images/female/winter-woman-crop2size.jpg')
+      });
+    } else if (gender === "male") {
+      this.setState({
+        img: require('../Images/male/winter-man-s.jpg')
+      });
+    } else {
+    this.setState({
+      img: require('../Images/defaultImg/winter-couplesize.jpg')
+    });
+  }
+  console.log("is working");
+}
+}
+
+
 componentDidUpdate(prevProps, prevState) {
   if (this.props.currentTemp !== prevProps.currentTemp) {
   this.setStyle();
   }
   console.log("image updated");
   console.log(this.state.img);
-}
-
-
-  setStyle(){
-  let currentTemp = this.props.currentTemp;
-  if ({currentTemp} >= 75) {
-    this.setState({
-    img: require('../Images/defaultImg/summer-groups.jpg')
-    });
-  } else if (({currentTemp} >= 60) & ({currentTemp} < 75)) {
-    this.setState({
-        img: require('../Images/defaultImg/spring-group-s.jpg')
-    });
-  } else if (({currentTemp} >= 50) & ({currentTemp} < 60)) {
-    this.setState({
-      img: require('../Images/defaultImg/fall-couple-s.jpg')
-    });
-  } else if (({currentTemp} >= 40) & ({currentTemp} < 50)) {
-    this.setState({
-      img: require('../Images/defaultImg/winter-couplesize.jpg')
-    });
-  } else {
-    this.setState({
-      img: require('../Images/defaultImg/winter-couplesize.jpg')
-    });
+  if (this.props.gender !== prevProps.gender) {
+  this.setStyle();
   }
-  console.log("is working");
 }
 
 
@@ -69,6 +87,7 @@ componentDidUpdate(prevProps, prevState) {
 
   }
 }
+
 
 
 export default Img;
